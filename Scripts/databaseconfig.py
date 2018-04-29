cd604fe57bf5956eeea56acaba67c5a7000349f1
Shreyas Ramakrishna <31136074+Shreyasramakrishna90@users.noreply.github.com>
2018-04-29 13:07:13 -0500

    Update databaseconfig.py

--- Scripts/databaseconfig.py
+++ Scripts/databaseconfig.py
@@ -7,8 +7,8 @@ config = Config()
 
 config.INFLUX_DBASE_HOST= "localhost" 
 config.INFLUX_DBASE_PORT=  "8086"
-config.INFLUX_DBASE_USER= "sterling"
-config.INFLUX_DBASE_PASSWORD=  "ranch"
+config.INFLUX_DBASE_USER= "distributed"
+config.INFLUX_DBASE_PASSWORD=  "system"
 config.INFLUX_DBASE_NAME= "newone"
 config.INFLUX_DBASE_RETENTION_POLICY="my policy"
 
